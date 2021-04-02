477e4bd2e27353bacdce08adead0ea9904a3e719
PPNeri <pedroneriti@gmail.com>
2021-04-02 17:39:18 -0300

    Updated

--- streamOfProducts/src/application/Program.java
+++ streamOfProducts/src/application/Program.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+import java.util.OptionalDouble;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -18,35 +19,41 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Enter full file path: ");
 		String path = sc.nextLine();
-		
+
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
 			List<Product> list = new ArrayList<>();
-			
+
 			String line = br.readLine();
+
 			while (line != null) {
 				String[] fields = line.split(",");
 				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
 				line = br.readLine();
 			}
-			
-			double avg = list.stream()
-					.map(p -> p.getPrice())
-					.reduce(0.0, (x,y) -> x + y) / list.size();
-			
-			System.out.println("Average price: " + String.format("%.2f", avg));
-			
-			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
-			
-			List<String> names = list.stream()
-					.filter(p -> p.getPrice() < avg)
-					.map(p -> p.getName()).sorted(comp.reversed())
-					.collect(Collectors.toList());
-			
-			names.forEach(System.out::println);
+			// Jeito Bonito de se Fazer
+
+			OptionalDouble average = list.stream().mapToDouble(Product::getPrice).average();
+			List<String> reversedNames = list.stream().filter(e -> e.getPrice() < average.getAsDouble())
+					.map(Product::getName).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+			System.out.println("Average price: " + String.format("%.2f", average.getAsDouble()));
+
+			reversedNames.forEach(System.out::println);
+
+			// double avg = list.stream().map(p -> p.getPrice()).reduce(0.0, (x, y) -> x +
+			// y) / list.size();
+			// System.out.println("Average price: " + String.format("%.2f", avg));
+
+			// Comparator<String> comp = (s1, s2) ->
+			// s1.toUpperCase().compareTo(s2.toUpperCase());
+//			List<String> names = list.stream().filter(p -> p.getPrice() < average.getAsDouble()).map(p -> p.getName())
+//					.sorted(comp.reversed()).collect(Collectors.toList());
+//			names.forEach(System.out::println);
+			System.out.println("----------------------");
 
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
